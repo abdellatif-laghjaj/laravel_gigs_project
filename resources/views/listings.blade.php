572901483554aabc95b7bac93be6cbfe517d69c9
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-07-18 00:01:35 +0100

    more blade

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,7 +1,15 @@
-<h1>
-    {{ $heading }}
+<h2>{{ $heading }}</h2>
+
+{{--@if(count($listings) === 0)--}}
+{{--    <p>No listings found yet</p>--}}
+{{--@endif--}}
+
+
+@unless(count($listings) == 0)
     @foreach($listings as $listing)
         <h1>{{$listing['title']}}</h1>
         <p>{{$listing['author']}}</p>
     @endforeach
-</h1>
+@else
+    <p>No listings found</p>
+@endunless
--- routes/web.php
+++ routes/web.php
@@ -33,41 +33,6 @@ Route::get('/', function () {
                 'title' => "Gig 3 Title",
                 'author' => "@abdel"
             ],
-            [
-                'id' => "4",
-                'title' => "Gig 4 Title",
-                'author' => "@abdel"
-            ],
-            [
-                'id' => "5",
-                'title' => "Gig 5 Title",
-                'author' => "@abdel"
-            ],
-            [
-                'id' => "6",
-                'title' => "Gig 6 Title",
-                'author' => "@abdel"
-            ],
-            [
-                'id' => "7",
-                'title' => "Gig 7 Title",
-                'author' => "@abdel"
-            ],
-            [
-                'id' => "8",
-                'title' => "Gig 8 Title",
-                'author' => "@abdel"
-            ],
-            [
-                'id' => "9",
-                'title' => "Gig 9 Title",
-                'author' => "@abdel"
-            ],
-            [
-                'id' => "10",
-                'title' => "Gig 10 Title",
-                'author' => "@abdel"
-            ],
         ]
     ]);
 });
