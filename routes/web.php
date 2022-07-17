8927202784e200651359692a32f955119f5ebbb4
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-07-17 21:34:59 +0100

    routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/about", function (){
+    return response("<h1>About Us</h1>")
+        ->header("content-type", "text/plain");
+});
