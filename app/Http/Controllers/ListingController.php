b5e86f097ff1fb996e2d6d5c5929393567ef8c1b
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-07-18 19:50:00 +0100

    searching

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -11,7 +11,7 @@ class ListingController extends Controller
     public static function index()
     {
         return view("listings.index", [
-            "listings" => Listing::latest()->filter(request(['tag']))->get(),
+            "listings" => Listing::latest()->filter(request(['tag', 'search']))->get(),
         ]);
     }
 
--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -15,5 +15,10 @@ class Listing extends Model
         if ($filters['tag'] ?? false) {
             $query->where('tags', 'like', '%' . request()->tag . '%');
         }
+        if ($filters['search'] ?? false) {
+            $query->where('title', 'like', '%' . request()->search . '%')
+                ->orWhere('description', 'like', '%' . request()->search . '%')
+                ->orWhere('tags', 'like', '%' . request()->search . '%');
+        }
     }
 }
