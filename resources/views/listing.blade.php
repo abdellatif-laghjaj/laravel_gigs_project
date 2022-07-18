b70fe99d79bf3829b8f65666367d8634ca5ecdf8
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-07-18 13:57:31 +0100

    list gigs

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,5 +1,9 @@
+@extends('layout')
+
+@section('content')
 <h1>{{$listing['title']}}</h1>
 <p>{{$listing['description']}}</p>
 <p>{{$listing['website']}}</p>
 <p>{{$listing['email']}}</p>
 <p>{{$listing['tags']}}</p>
+@endsection
--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,4 +1,8 @@
-<h2>{{ $heading }}</h2>
+@extends('layout')
+
+@section('content')
+<div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+
 
 {{--@if(count($listings) === 0)--}}
 {{--    <p>No listings found yet</p>--}}
@@ -7,11 +11,42 @@
 
 @unless(count($listings) == 0)
     @foreach($listings as $listing)
-        <h1>
-            <a href="/listing/{{$listing['id']}}">{{$listing['title']}}</a>
-        </h1>
-        <p>{{$listing['description']}}</p>
+            <!-- Item 1 -->
+            <div class="bg-gray-50 border border-gray-200 rounded p-6">
+                <div class="flex">
+                    <img class="hidden w-48 mr-6 md:block" src="images/acme.png" alt="" />
+                    <div>
+                        <h3 class="text-2xl">
+                            <a href="/listing/{{$listing->id}}">{{$listing->title}}</a>
+                        </h3>
+                        <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
+                        <ul class="flex">
+                            <li
+                                class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+                                <a href="#">Laravel</a>
+                            </li>
+                            <li
+                                class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+                                <a href="#">API</a>
+                            </li>
+                            <li
+                                class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+                                <a href="#">Backend</a>
+                            </li>
+                            <li
+                                class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+                                <a href="#">Vue</a>
+                            </li>
+                        </ul>
+                        <div class="text-lg mt-4">
+                            <i class="fa-solid fa-location-dot"></i> {{$listing->location}}
+                        </div>
+                    </div>
+                </div>
+            </div>
     @endforeach
 @else
     <p>No listings found</p>
 @endunless
+</div>
+@endsection
