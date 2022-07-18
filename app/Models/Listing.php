d5b9caa27875219665b05f13a7da1fa7438a4bc3
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-07-18 11:46:25 +0100

    create model

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,31 +2,10 @@
 
 namespace App\Models;
 
-class Listing{
-    public static function all(): array{
-        return [
-            [
-                'id' => "1",
-                'title' => "Gig 1 Title",
-                'author' => "@abdel"
-            ],
-            [
-                'id' => "2",
-                'title' => "Gig 2 Title",
-                'author' => "@abdel"
-            ],
-            [
-                'id' => "3",
-                'title' => "Gig 3 Title",
-                'author' => "@abdel"
-            ],
-        ];
-    }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-    public static function find($id){
-        $listings = self::all();
-        foreach ($listings as $listing) {
-            if($listing['id'] == $id) return $listing;
-        }
-    }
+class Listing extends Model
+{
+    use HasFactory;
 }
