a259dd08188ca62114037b24321910378efd0ae4
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-07-19 23:54:36 +0100

    bug fixes

--- database/migrations/2022_07_18_001634_create_listings_table.php
+++ database/migrations/2022_07_18_001634_create_listings_table.php
@@ -14,6 +14,7 @@ return new class extends Migration {
     {
         Schema::create('listings', function (Blueprint $table) {
             $table->id();
+
             $table->string('title');
             $table->string('logo')->nullable();
             $table->string('tags');
