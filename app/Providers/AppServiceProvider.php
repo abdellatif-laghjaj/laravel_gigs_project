af5dbba8646803566aa601b4d56f1330d29b8750
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-07-19 13:20:46 +0100

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
