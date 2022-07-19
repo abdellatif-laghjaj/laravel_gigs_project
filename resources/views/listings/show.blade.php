6ca853702d696b4314001e8a39c737aa5f97cf80
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-07-19 13:55:44 +0100

    display images

--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -7,12 +7,14 @@
     <div class="mx-4">
         <x-card class="p-10">
             <div class="flex flex-col items-center justify-center text-center">
-                <img class="w-48 mr-6 mb-6" src="{{asset('images/no-image.png')}}" alt=""/>
+                <img class="w-48 mr-6 mb-6"
+                     src="{{asset($listing->logo ? "storage/".$listing->logo : "images/no-image.png")}}"
+                     alt="Listing Logo"/>
 
                 <h3 class="text-2xl mb-2">{{$listing->title}}</h3>
                 <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
                 {{-- TAGS --}}
-                <x-listing-tags :tagsCsv="$listing->tags"/>
+                <x-listing-tags :tagsCsv="$listing->tags"></x-listing-tags>
                 <div class="text-lg my-4">
                     <i class="fa-solid fa-location-dot"></i> {{$listing->location}}
                 </div>
