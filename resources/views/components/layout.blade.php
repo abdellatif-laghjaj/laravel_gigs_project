1436a9da3071e46d3799b8d77caed2ef76d34567
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-07-19 22:04:16 +0100

    if user is logged show welcome

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -2,72 +2,75 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="icon" href="images/favicon.ico" />
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
-    integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <script src="//unpkg.com/alpinejs" defer></script>
-  <script src="https://cdn.tailwindcss.com"></script>
-  <script>
-    tailwind.config = {
-        theme: {
-          extend: {
-            colors: {
-              laravel: '#ef3b2d',
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="icon" href="images/favicon.ico"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+          integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+          crossorigin="anonymous" referrerpolicy="no-referrer"/>
+    <script src="//unpkg.com/alpinejs" defer></script>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+        tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        laravel: '#ef3b2d',
+                    },
+                },
             },
-          },
-        },
-      }
-  </script>
-  <title>LaraGigs | Find Laravel Jobs & Projects</title>
+        }
+    </script>
+    <title>LaraGigs | Find Laravel Jobs & Projects</title>
 </head>
 
 <body class="mb-48">
-  <nav class="flex justify-between items-center mb-4">
-    <a href="/"><img class="w-24" src="{{asset('images/logo.png')}}" alt="" class="logo" /></a>
+<nav class="flex justify-between items-center mb-4">
+    <a href="/"><img class="w-24" src="{{asset('images/logo.png')}}" alt="" class="logo"/></a>
     <ul class="flex space-x-6 mr-6 text-lg">
-      @auth
-      <li>
-        <span class="font-bold uppercase">
-          Welcome {{auth()->user()->name}}
-        </span>
-      </li>
-      <li>
-        <a href="/listings/manage" class="hover:text-laravel"><i class="fa-solid fa-gear"></i> Manage Listings</a>
-      </li>
-      <li>
-        <form class="inline" method="POST" action="/logout">
-          @csrf
-          <button type="submit">
-            <i class="fa-solid fa-door-closed"></i> Logout
-          </button>
-        </form>
-      </li>
-      @else
-      <li>
-        <a href="/register" class="hover:text-laravel"><i class="fa-solid fa-user-plus"></i> Register</a>
-      </li>
-      <li>
-        <a href="/login" class="hover:text-laravel"><i class="fa-solid fa-arrow-right-to-bracket"></i> Login</a>
-      </li>
-      @endauth
+        {{-- If user is logged in --}}
+        @auth()
+            <li>
+            <span class="font-bold uppercase">
+                Welcome, <span class="text-laravel">{{auth()->user()->name}}</span>
+            </span>
+            </li>
+            <li>
+                <a href="/listings/manage" class="hover:text-laravel">
+                    <i class="fa-solid fa-bars-progress"></i> Manage Listings
+                </a>
+            </li>
+            <li>
+                <form action="/logout" method="post">
+                    @csrf
+                    <button type="submit">
+                        <i class="fa-solid fa-power-off"></i> Logout
+                    </button>
+                </form>
+            </li>
+        @else
+            <li>
+                <a href="/register" class="hover:text-laravel"><i class="fa-solid fa-user-plus"></i> Register</a>
+            </li>
+            <li>
+                <a href="/login" class="hover:text-laravel"><i class="fa-solid fa-arrow-right-to-bracket"></i> Login</a>
+            </li>
+        @endauth
     </ul>
-  </nav>
+</nav>
 
-  <main>
+<main>
     {{$slot}}
-  </main>
-  <footer
+</main>
+<footer
     class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center">
     <p class="ml-2">Copyright &copy; 2022, All Rights reserved</p>
 
     <a href="/listings/create" class="absolute top-1/3 right-10 bg-black text-white py-2 px-5">Post Job</a>
-  </footer>
+</footer>
 
-  <x-flash-message />
+<x-flash-message/>
 </body>
 
 </html>
