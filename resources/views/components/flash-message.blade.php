78831632f11fcccebdddd13ea5d04c8aa7704808
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-07-19 01:18:16 +0100

    show message

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,5 +1,6 @@
 @if(session()->has('message'))
-    <div class="fixed top-0 left-1/2 text-white px-6 py-3 -translate-x-1/2 transform bg-laravel">
+    <div x-data="{show: true}" x-init="setTimeout(() => show = false, 3000)" x-show="show"
+         class="fixed top-0 left-1/2 text-white px-6 py-3 -translate-x-1/2 transform bg-green-500">
         <p>
             {{session('message')}}
         </p>
